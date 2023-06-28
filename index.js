69ad98f88e91a4af36f717967d64c53ba9a59572
John Eric Lupisan <john.lupisan@gmail.com>
2023-06-27 23:00:55 -0800

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const body = document.querySelector('body')
+
+function submitData(name, email){
+    const url = "http://localhost:3000/users";
+    return fetch(url, {
+        method:'POST',
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.body.innerHTML = data['id']
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+
+   })
+}
